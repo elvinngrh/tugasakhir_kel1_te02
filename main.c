76e7490ab66ff4b423d48303a9ec7d4c85824515
nurwidiatamafauzanbn <nurwidiatamafauzanbn@student.upi.edu>
2025-12-15 05:59:38 +0700

    Update main.c

--- main.c
+++ main.c
@@ -5,33 +5,51 @@ void DaftarFilm() {
 }
 
 int main() {
+    char film[5][20] = {
+        "A",
+        "B",
+        "C",
+        "D",
+        "E"
+    };
 
-    int jumlah_film = 5;
-    int No_film = jumlah_film;
-    char film[5][30] = {
+    char rating_usia[5][5] = {
+        "13+",
+        "21+",
+        "2+",
+        "2+",
+        "13+"
+    };
 
-    }; /*Ini misalkan filmnya*/
+    char genre_film[5][20] = {
+        "Horror",
+        "Komedi Dewasa",
+        "Anak",
+        "Anak",
+        "Thriller"
+    };
 
-    char rating_usia[5][30] = {
+    int durasi[5] = {
+        90,
+        30,
+        120,
+        60,
+        120
+    };
+    char tayang[5][20] = {
+        "19:30/02/07/2025", 
+        "kecha",
+        "kecha",
+        "kecha",
+        "kecha"};
 
-    }; /*Rating usia*/
-
-    char genre_film[5][30] = {
-
-    }; /*genre film, boleh lebih dari satu*/
-
-        int durasi_film[5][30] = {
-
-    }; /*Durasi*/
-
-
-    printf("===================================================\n");
-    printf("| %-2s | %-8s | %-8s | %-6s | %-5s |\n", "NO", "Judul Film", "Rating Usia", "Genre", "Durasi");
-    printf("===================================================\n");
-
+    printf("====================================================================================================================\n");
+    printf("| %-2s | %-30s | %-8s | %-20s | %-10s | %-20s |\n", "NO", "Judul Film", "Rating Usia", "Genre", "Durasi (menit)", "Tayang Perdana");
+    printf("====================================================================================================================\n");
     for (int i; i < jumlah_film; i++) {
-        printf("| %-2d | %-8s | %-8s | %-6s | %-5s |\n", i, film[i], rating_usia[i], genre_film[i], durasi_film[i]);
-
+        printf("| %-2d | %-30s | %-11s | %-20s | %-14d | %-20s |\n", i+1, film[i], rating_usia[i], genre_film[i], durasi[i], tayang[i]);
     }
+    printf("====================================================================================================================\n");
 
+    return 0;
 }
