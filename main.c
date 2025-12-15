d574db61881a7591c6103dcecaa762c2376c762d
nurwidiatamafauzanbn <nurwidiatamafauzanbn@student.upi.edu>
2025-12-15 09:48:22 +0700

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ struct Film {
     char judul_film[30];
     char rating_usia[5];
     char genre[15];
-    int durasi;
+    int harga;
     char tayang[20];
 };
 
@@ -23,7 +23,7 @@ int main() {
     };
 
     printf("=====================================================================================================================\n");
-    printf("| %-2s | %-30s | %-12s | %-20s | %-14s | %-20s |\n", "NO", "Judul Film", "Rating Usia", "Genre", "Durasi (menit)", "Tayang Perdana");
+    printf("| %-2s | %-30s | %-12s | %-20s | %-14s | %-20s |\n", "NO", "Judul Film", "Rating Usia", "Genre", "Harga (Rp)", "Tayang Perdana");
     printf("=====================================================================================================================\n");
     for (int i = 0; i < jumlah_film; i++) {
         printf("| %-2d | %-30.30s | %-12.12s | %-20.20s | %-14d | %-20.20s |\n", 
@@ -31,7 +31,7 @@ int main() {
             daftar_film[i].judul_film, 
             daftar_film[i].rating_usia, 
             daftar_film[i].genre, 
-            daftar_film[i].durasi, 
+            daftar_film[i].harga, 
             daftar_film[i].tayang);
     }
     printf("=====================================================================================================================\n");
