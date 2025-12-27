8899ec84ccfb0d8702ee095440f3ca47b9800be3
elvinngrh <elvinnugraha25@student.upi.edu>
2025-12-27 23:49:55 +0700

    Update program.c

--- program.c
+++ program.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 #define JUMLAH_FILM 5
 
@@ -20,24 +19,17 @@ struct Booking {
     int id;
     char pemesan[30];
     char judul[30];
-    char jadwal_tayang[15];
+    char jadwal_tayang[20];
     int jumlah_tiket;
     int total_pembayaran;
 };
 
-void Daftar_Film(struct Film film[]) { 
-
+void Daftar_Film(struct Film film[]) {
     char judul_film[JUMLAH_FILM][20] = {"Interstellar", "Avengers", "C", "D", "E"};
     char rating_usia[JUMLAH_FILM][20] = {"13+", "21+", "2+", "2+", "13"};
-    char genre[JUMLAH_FILM][20] = {"Horror", "Komedi dewasa", "Anak", "Anak", "Thriller"};
-    char harga[JUMLAH_FILM][20] = {"90000", "120000", "30.000", "65000", "100000"};
-    char tanggal[JUMLAH_FILM][20] = {
-        "19//07/2025",
-        "kecha",
-        "kecha",
-        "kecha", 
-        "kecha"
-    };
+    char genre[JUMLAH_FILM][20] = {"Horror", "Komedi", "Anak", "Anak", "Thriller"};
+    char harga[JUMLAH_FILM][20] = {"90000", "120000", "30000", "65000", "100000"};
+    char tanggal[JUMLAH_FILM][20] = {"19/07/2025", "20/07/2025", "21/07/2025", "22/07/2025", "23/07/2025"};
 
     for (int i = 0; i < JUMLAH_FILM; i++) {
         strcpy(film[i].judul_film, judul_film[i]);
@@ -49,74 +41,69 @@ void Daftar_Film(struct Film film[]) {
 }
 
 int total_harga(int pilihan, int jumlah_tiket) {
-    struct Film daftar_film[JUMLAH_FILM];
-    Daftar_Film(daftar_film);
-    int harga_tiket = 0;
-    int jumlah = jumlah_tiket;
-
-    if (pilihan >= 1 && pilihan <= JUMLAH_FILM) {
-        char *harga_str = daftar_film[pilihan - 1].harga;
-        harga_tiket = atoi(harga_str);
-    }
-
-    return jumlah*harga_tiket;
+    struct Film film[JUMLAH_FILM];
+    Daftar_Film(film);
+    return atoi(film[pilihan - 1].harga) * jumlah_tiket;
 }
 
 int add_booking(int pilihan) {
-    struct Film daftar_film[JUMLAH_FILM];
-    Daftar_Film(daftar_film);
+    struct Film film[JUMLAH_FILM];
+    Daftar_Film(film);
+
     struct Booking b1;
-    char buffer[100];
-    
-    FILE *fp = fopen("booking.txt", "a");
-    if (fp == NULL) {
+    char buffer[50];
+
+    FILE *fp = fopen("booking.txt", "a+");
+    if (!fp) {
         printf("Gagal membuka file!\n");
         return 1;
     }
-    int ch;
-    while ((ch = getchar()) != '\n' && ch != EOF);
 
-    printf("Nama Pemesan         : ");
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+
+    if (size == 0) {
+        fprintf(fp, "==================================================================================================================================\n");
+        fprintf(fp, "| %-2s | %-30s | %-25s | %-20s | %-14s | %-20s |\n",
+                "ID", "Nama", "Judul Film", "Jadwal", "Jumlah Tiket", "Total Biaya");
+        fprintf(fp, "==================================================================================================================================\n");
+    }
+
+    getchar();
+
+    printf("Nama Pemesan : ");
     fgets(b1.pemesan, sizeof(b1.pemesan), stdin);
-    b1.pemesan[strcspn(b1.pemesan, "\n")] = '\0';
+    b1.pemesan[strcspn(b1.pemesan, "\n")] = 0;
 
-    printf("Jumlah Tiket         :");
+    printf("Jumlah Tiket : ");
     fgets(buffer, sizeof(buffer), stdin);
-    b1.jumlah_tiket = strtol(buffer, NULL, 10);
-
-    strcpy(b1.judul, daftar_film[pilihan-1].judul_film);
-    strcpy(b1.jadwal_tayang, daftar_film[pilihan-1].tayang);
-    b1.id = pilihan;
-    b1.total_pembayaran = total_harga(pilihan, b1.jumlah_tiket);
+    b1.jumlah_tiket = atoi(buffer);
 
     if (b1.jumlah_tiket <= 0) {
         printf("Jumlah tiket tidak valid!\n");
         fclose(fp);
         return 1;
     }
+    strcpy(b1.judul, film[pilihan - 1].judul_film);
+    strcpy(b1.jadwal_tayang, film[pilihan - 1].tayang);
+    b1.id = pilihan;
+    b1.total_pembayaran = total_harga(pilihan, b1.jumlah_tiket);
+    fprintf(fp, "| %-2d | %-30s | %-25s | %-20s | %-14d | Rp.%-17d |\n",
+            b1.id,
+            b1.pemesan,
+            b1.judul,
+            b1.jadwal_tayang,
+            b1.jumlah_tiket,
+            b1.total_pembayaran);
 
     fprintf(fp, "==================================================================================================================================\n");
-    fprintf(fp, "| %-2s | %-30s | %-25s | %-20s | %-14s | %-20s |\n",
-            "ID", "Nama", "Judul FILM", "Jadwal Tayang", "Jumlah Tiket", "Total Pembayaran");
-    fprintf(fp, "==================================================================================================================================\n");
-
-    fprintf(fp, "| %-2d | %-30s | %-25s | %-20s | %-14d | %-20d |\n",
-        b1.id,
-        b1.pemesan,
-        b1.judul,
-        b1.jadwal_tayang,
-        b1.jumlah_tiket,
-        b1.total_pembayaran);
-
-    fprintf(fp, "==================================================================================================================================\n");
-
     fclose(fp);
-    printf("\nbooking berhasil ditambahkan!\n");
+    printf("Booking berhasil disimpan!\n");
     return 0;
 }
 
 void pilih_film(int *pilihfilm) {
-    char ver;
+   char ver;
     do {
     printf("\nSilahkan pilih film berdasarkan nomor urut (1 - %d):", JUMLAH_FILM);
     scanf("%d", pilihfilm);
@@ -128,7 +115,7 @@ void pilih_film(int *pilihfilm) {
     } while (*pilihfilm < 1 || *pilihfilm > JUMLAH_FILM);
 
     printf("Apakah anda sudah yakin dengan pilihan anda? Y/N:");
-    scanf("%s", &ver);
+    scanf(" %c", &ver);
     
     do {
         if (ver != 'Y' && ver != 'N') {
@@ -148,43 +135,42 @@ void pilih_film(int *pilihfilm) {
 }
 
 void tabel_film() {
-    struct Film daftar_film[JUMLAH_FILM];
-    Daftar_Film(daftar_film);
+    struct Film film[JUMLAH_FILM];
+    Daftar_Film(film);
 
-    printf("=====================================================================================================================\n");
-    printf("| %-2s | %-30.30s | %-12.12s | %-20.20s | %-14.14s | %-20.20s |\n", "NO", "Judul Film", "Rating Usia", "Genre", "Harga (Rp)", "Tayang Perdana");
-    printf("=====================================================================================================================\n");
+    printf("\n====================================================================================================\n");
+    printf("| %-2s | %-30s | %-10s | %-15s | %-10s | %-15s |\n",
+           "No", "Judul", "Rating", "Genre", "Harga", "Tayang");
+    printf("====================================================================================================\n");
+
     for (int i = 0; i < JUMLAH_FILM; i++) {
-        printf("| %-2d | %-30s | %-12s | %-20s | %-14s | %-20s |\n", 
-            i+1,
-            daftar_film[i].judul_film, 
-            daftar_film[i].rating_usia, 
-            daftar_film[i].genre,
-            daftar_film[i].harga, 
-            daftar_film[i].tayang);
+        printf("| %-2d | %-30s | %-10s | %-15s | Rp.%-7s | %-15s |\n",
+               i + 1,
+               film[i].judul_film,
+               film[i].rating_usia,
+               film[i].genre,
+               film[i].harga,
+               film[i].tayang);
     }
-    printf("=====================================================================================================================\n");
+    printf("====================================================================================================\n");
 }
 
 int main() {
-    struct Film daftar_film[JUMLAH_FILM];
-    Daftar_Film(daftar_film);
-    int pilihan_film;
-    int pilih;
-    
-    do {
-        printf("\n1. Lihat Film dan Jadwal");
-        printf("\n2. Booking Tiket");
-        printf("\n3. Lihat Lengkap");
+    int pilih, film;
 
-        printf("\n0. Keluar\nPilih: ");
+    do {
+        printf("\n=== MENU ===");
+        printf("\n1. Lihat Film");
+        printf("\n2. Booking Tiket");
+        printf("\n0. Keluar");
+        printf("\nPilih: ");
         scanf("%d", &pilih);
 
         if (pilih == 1) tabel_film();
-        else if (pilih == 2) pilih_film(&pilihan_film);
-        else if (pilih == 3) printf("\nFitur belum tersedia\n");
-        else if (pilih == 0) printf("\nTerima kasih telah menggunakan layanan kami!\n");
-        else printf("\nPilihan tidak valid, silakan coba lagi.\n");
+        else if (pilih == 2) pilih_film(&film);
+        else if (pilih == 0) printf("Terima kasih!\n");
+        else printf("Pilihan tidak valid!\n");
+
     } while (pilih != 0);
 
     return 0;
